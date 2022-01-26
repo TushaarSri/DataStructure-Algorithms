6ece0dc2a15bc168e26f381844192f748bd1d5a8
Tushaar Srivastava <tushaar.gcsrivastava@gmail.com>
2022-01-26 23:36:27 +0530

    Initial commit

---
+++ src/com/ds/arrays/WithArray.java
@@ -0,0 +1,15 @@
+package com.ds.arrays;
+
+public class WithArray {
+
+    public static void main(String[] args) {
+        String Students[] = {"student1", "student2", "student3"};
+
+    }
+
+    public static void printStudentList(String[] students){
+        for(String s : students){
+            System.out.println(s);
+        }
+    }
+}
