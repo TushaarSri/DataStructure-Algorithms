b6c549e4747b6854aa3fa2cb85198046e24bc496
Tushaar Srivastava <tushaar.gcsrivastava@gmail.com>
2022-01-30 20:22:09 +0530

    Add. remove, contain in ArrayList

--- src/com/ds/arraylist/ArrayListLearning.java
+++ src/com/ds/arraylist/ArrayListLearning.java
@@ -1,45 +1,41 @@
 package com.ds.arraylist;
-import  java.util.ArrayList;
+
+import java.util.ArrayList;
 
 public class ArrayListLearning {
 
     public static void main(String[] args) {
-        /*Student studentList[] = { issue here we are restricted with the size
-        How ArrayList rescue us?
-                new Student("name1", 01),
-                new Student("name2", 02),
-                new Student("name3", 03)
-        };*/
         ArrayList<Student> studentList = new ArrayList<Student>();
         studentList.add(new Student("name1", 01));
         studentList.add(new Student("name2", 02));
-        studentList.add(new Student("name3", 03));
+        /* adding values to particular index */
+        studentList.add(0, new Student("name3", 03));
         studentList.add(new Student("Hari", 04));
-        studentList.add(new Student("Ravishankar", 05));
-
+        /* remove the value from List */
+        studentList.remove(3);
 
         printStudentList(studentList);
     }
 
-    public static void printStudentList(ArrayList<Student> students){
+    public static void printStudentList(ArrayList<Student> students) {
 
 
-        for(Student s : students){
+        for (Student s : students) {
             System.out.println(s.getDetails());
         }
     }
 }
 
-class Student{
+class Student {
     private final String name;
     private final int rollNumber;
 
-    public Student(String name, int rollNumber){
+    public Student(String name, int rollNumber) {
         this.name = name;
         this.rollNumber = rollNumber;
     }
 
-    public String getDetails(){
-        return "name  =" +this.name + '\n' + "roll Number =" + this.rollNumber;
+    public String getDetails() {
+        return "name  =" + this.name + '\n' + "roll Number =" + this.rollNumber;
     }
 }
